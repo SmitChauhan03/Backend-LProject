8f53c6d1703bdcca79391bef790573e704100808
smit <smitwebdev03@gmail.com>
2026-04-14 16:18:31 +0530

    LOGIN/LOGOUT DONE

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,6 +5,22 @@ import { uploadOnCloudinary } from '../utils/cloudinary.js';
 import { ApiResponse } from '../utils/ApiResponse.js';
 
 
+const generateAccessAndRefreshTokens = async (userId) => {
+    try{
+        const user = await User.findById(userId);
+        const accessToken = user.generateAccessToken(); 
+        const refreshToken = user.generateRefreshToken();
+        user.refreshToken = refreshToken
+        await user.save({ validateBeforeSave : false })
+
+        
+        return {accessToken, refreshToken}
+
+    }catch(error){
+        console.log("🔥 FULL TOKEN ERROR:", error);
+        throw new ApiError(500, "Something went wrong while generating refresh and access tocken.")
+    }
+}
 
 const registerUser = asyncHandler( async (req, res) => {
     
@@ -92,9 +108,95 @@ const registerUser = asyncHandler( async (req, res) => {
     
 } )
 
+const loginUser  = asyncHandler( async (req, res) => {
+
+    // req body data
+    const {email, username, password} = req.body
+
+    // username  or email 
+    if(!(username && email)){
+        throw new ApiError(400, "username or email is required !!")
+    }
+    
+    // find the user
+    const user = await User.findOne({ 
+        $or: [ {username}, {email}]
+    })
+    if(!user){
+        throw new ApiError(404, "User does not exist.")
+    }
+
+    // password check
+    const isPasswordValid = await user.isPasswordCorrect(password);
+
+    if(!isPasswordValid){
+        throw new ApiError(401, "Invalid user Password.")
+    };
+
+
+    // access and refresh token
+    const {accessToken, refreshToken } = await generateAccessAndRefreshTokens(user._id);
+
+    const loggedInUser = await User.findById(user._id).select(" -password -refreshToken "); 
+    // console.log("TYPE OF LOGGED IN USER:", typeof loggedInUser);
+    // console.log("IS PLAIN ", loggedInUser.constructor.name)
+
+    // send cookies
+    const options = {
+        httpOnly : true,
+        secure : true,
+    }
+
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+        // new ApiResponse(
+        //     200, 
+        //     {
+        //         user: loggedInUser, accessToken, refreshToken
+        //     },
+        //     "User logged In Successfully"
+        // )
+        {
+            user: loggedInUser, accessToken, refreshToken 
+        }
+    )
+
+} );
+
+const logoutUser = asyncHandler( async (req, res ) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+    )
+    const options = {
+        httpOnly : true,
+        secure : true,
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(
+        new ApiResponse(200, 
+            {}, "User looged Out!"
+        )
+    )
+})
 
 
 
 
 
-export {registerUser}
+
+export {registerUser, loginUser, logoutUser}
